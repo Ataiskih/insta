fa1c7398a7523b526079220c6510d0b5bb2d82c5
fox <fox@localhost.localdomain>
2020-09-23 15:06:13 +0600

    added admin pannel

---
+++ main/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from main.models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [
+        'pk', 'user',
+        'created', 'updated',
+        'deleted'
+    ]
+    fields = [
+        'user'
+    ]
---
+++ main/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+# this model like a default fields for another models
+class BaseAbstractModel(models.Model):
+    name = models.CharField(
+        max_length=255,
+        blank=True, null=True,
+        verbose_name='Name'
+    )
+    created = models.DateField(
+        auto_now_add=True,
+        verbose_name='Created date'
+    )
+    updated = models.DateField(
+        auto_now=True,
+        verbose_name='Updated date'
+    )
+    deleted = models.BooleanField(
+        default=False,
+        verbose_name='Deleted'
+    )
+
+    def __str__(self):
+        if self.name:
+            return self.name
+        return f"{self.pk}"
+
+    class Meta:
+        abstract = True
+
+class Profile(BaseAbstractModel):
+    user = models.OneToOneField(
+        User, on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='profile',
+        verbose_name='User'
+    )
